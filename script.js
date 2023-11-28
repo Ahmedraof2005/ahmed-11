ff2eabde4bebdd7746b6734b14a48262cc97624c
Ahmedraof2005 <ahmedabdelraof189@gmail.com>
2023-11-28 22:18:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+
+
+/*
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector("#green").onclick = function(){
+    document.querySelector("#name").style.color = "green";
+  }
+
+  
+  document.querySelector("#red").onclick = function(){
+    document.querySelector("#name").style.color = "red";
+  }
+
+  
+  document.querySelector("#yellow").onclick = function(){
+    document.querySelector("#name").style.color = "yellow";
+  }
+  
+  
+
+} )   
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+document.addEventListener("DOMContentLoaded" , function(){
+   document.querySelector("#submit").disabled = true;
+   document.querySelector("#task").onkeyup = function(){
+    if(document.querySelector("#task"))
+   }
+}
+)
+
+
+             */
+
+
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(  getElementById("name").innerHtml = data[0].name );
+
+
+
+
+
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => console.log(data[1].email));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
